ca68ba594e17d3f4cb5348d3fa438d16fd0d20cd
Francesco Saverio <frappa@FrappaPC.localdomain>
2025-07-28 15:11:42 +0200

    Riorganizzata struttura di base React e rimossi file statici

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1 className="title">
+        Francesco<br />Saverio
+      </h1>
+      <p className="subtitle">Cybersecurity Student &amp; Dev</p>
+    </section>
+  );
+}
