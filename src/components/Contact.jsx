463fe8c6fcfda9df49b1de4d331e575c7897c267
Frappa251 <francescosaverio2004@gmail.com>
2026-05-27 15:56:19 +0200

    New Fresh Start

--- src/components/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-    return(
-        <section id="contact">
-            <h2>Contatti</h2>
-            <p>Email, Githu, Telegram (qui ci metterai le cose)</p>
-        </section>
-    );
-}
